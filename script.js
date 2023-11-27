65bd3bbbfb59e68c3925704212185c9d01d26291
moonleeks <moonleeks@proton.me>
2023-11-27 12:49:21 +0200

    base square size on view height instead of width

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const gridContainer = document.querySelector(".gridContainer");
-let squaresPerSide = 5; 
+let squaresPerSide = 4; 
 let totalSquares = squaresPerSide ** 2;
-let cardWidth = ((100 / squaresPerSide) -2) + "vw";
+let cardHeigth = ((90 / squaresPerSide) -3) + "vh";
 
 // one node can oly be added t othe DOM once, therefore the nodes have to be
 // created inside the for loop
 for (let i = 0; i < totalSquares; i++) {    
     const card = document.createElement('div');
     card.classList.add('card');
-    card.style.width = cardWidth;
+    card.style.height = cardHeigth;
     gridContainer.appendChild(card);
 }
