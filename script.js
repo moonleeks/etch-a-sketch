4bd386dcefeda55c521a5b055a5a50afca48b7df
moonleeks <moonleeks@proton.me>
2023-11-27 15:48:41 +0200

    add hovering functionality

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const gridContainer = document.querySelector(".gridContainer");
 let squaresPerSide = 4; 
 let totalSquares = squaresPerSide ** 2;
 let cardHeigth = ((90 / squaresPerSide) -3) + "vh";
 
 // one node can oly be added t othe DOM once, therefore the nodes have to be
 // created inside the for loop
+const gridContainer = document.querySelector(".gridContainer");
 for (let i = 0; i < totalSquares; i++) {    
     const card = document.createElement('div');
     card.classList.add('card');
-    card.style.height = cardHeigth;
+    card.style.height = cardHeigth;    
     gridContainer.appendChild(card);
 }
+
+/* const cards = document.querySelectorAll('.card');
+cards.forEach(card, () => {
+    this.addEventListener('hover', () => {
+        
+    });
+});
+
+function onCardHover() {
+    
+} */
+
+const body = document.querySelector('body');
+body.addEventListener('mouseover', function (e) {
+    if(e.target.classList.contains('card')) {
+        e.target.style.backgroundColor = 'red';
+    }
+});
