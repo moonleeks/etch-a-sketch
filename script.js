51c7be0c7496be90075d2d8b71c01da6a2d82cb7
moonleeks <moonleeks@proton.me>
2023-11-27 17:56:54 +0200

    add generate button, move gen logic to a function

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-let squaresPerSide = 4; 
-let totalSquares = squaresPerSide ** 2;
-let cardHeigth = ((90 / squaresPerSide) -3) + "vh";
-
-// one node can oly be added t othe DOM once, therefore the nodes have to be
-// created inside the for loop
 const gridContainer = document.querySelector(".gridContainer");
-for (let i = 0; i < totalSquares; i++) {    
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.style.height = cardHeigth;    
-    gridContainer.appendChild(card);
-}
-
-/* const cards = document.querySelectorAll('.card');
-cards.forEach(card, () => {
-    this.addEventListener('hover', () => {
-        
-    });
-});
-
-function onCardHover() {
-    
-} */
+generateGrid(20);
 
 const body = document.querySelector('body');
 body.addEventListener('mouseover', function (e) {
@@ -29,3 +7,35 @@ body.addEventListener('mouseover', function (e) {
         e.target.style.backgroundColor = 'red';
     }
 });
+body.addEventListener('mouseout', function (e) {
+    if(e.target.classList.contains('card')) {
+        e.target.style.backgroundColor = 'cornflowerblue';
+    }
+});
+
+const genButton = document.querySelector('button');
+genButton.addEventListener('click', () => {
+    squaresPerSide = parseInt(prompt("Please enter number of squares per side"));
+    
+    //regenerate
+})
+
+function generateGrid(squaresPerSide) {
+    //clear existing cards
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(e => e.remove());
+
+    let totalSquares = squaresPerSide ** 2;
+    let cardHeigth = ((90 / squaresPerSide) -3) + "vh";
+
+    // one node can only be added to the DOM once, therefore the nodes have to be
+    // created inside the for loop
+    for (let i = 0; i < totalSquares; i++) {    
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.style.height = cardHeigth;    
+        gridContainer.appendChild(card);
+    }
+    
+    
+}
